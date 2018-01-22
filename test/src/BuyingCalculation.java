e8ab47639580587b82ee6f9e17623d1549c648fa
guangliang0209 <fgliang641397267@163.com>
2018-01-22 11:12:45 +0800

    读取PDF文件

--- test/src/BuyingCalculation.java
+++ test/src/BuyingCalculation.java
@@ -3,6 +3,7 @@ import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 
 import java.io.IOException;
 
+
 /**
  * 摇号买房概率计算
  *
@@ -15,21 +16,28 @@ public class BuyingCalculation {
 
         try {
             reader = new PdfReader(pdfPath);
-            int num = reader.getNumberOfPages();// 获得页数
-
+            // 获得页数
+            int num = reader.getNumberOfPages();
             System.out.println("Total Page: " + num);
 
-            String content = ""; // 存放读取出的文档内容
+            // 存放读取出的文档内容
+            String content = "";
 
             for (int i = 1; i <= num; i++) {
                 content += "\n";
                 content += PdfTextExtractor.getTextFromPage(reader, i);
             }
 
-            String[] arr = content.split("/n");
+            String[] arr = content.split("\n");
 
             for (int i = 0; i < arr.length; i++) {
-                System.out.println(arr[i]);
+                String temp = arr[i].trim();
+                String[] tempArr = temp.split(" ");
+                if (tempArr.length > 2) {
+                    System.out.println(tempArr[1] + " " + tempArr[2]);
+                } else {
+                    System.out.println(arr[i]);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
